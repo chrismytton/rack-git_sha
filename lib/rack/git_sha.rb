f7a8ac3c03b3131626cd263276602c8eb5b45430
Chris Mytton <self@hecticjeff.net>
2012-01-18 19:18:39 +0000

    Check for a revision file

--- lib/rack/git_sha.rb
+++ lib/rack/git_sha.rb
@@ -1,9 +1,28 @@
 require 'rack/git_sha/version'
+require 'pathname'
 
 module Rack
   class GitSha
+    def initialize(root = Dir.pwd)
+      @root = Pathname.new(root)
+    end
+
     def call(env)
-      [200, {'Content-Type' => 'text/plain'}, [`git rev-parse HEAD`]]
+      if revision_file.exist?
+        [200, headers, [revision_file.read]]
+      else
+        [200, headers, [`git rev-parse HEAD`]]
+      end
+    end
+
+  private
+
+    def revision_file
+      @root.join('REVISION')
+    end
+
+    def headers
+      {'Content-Type' => 'text/plain'}
     end
   end
 end
--- spec/rack-git_sha_spec.rb
+++ spec/rack-git_sha_spec.rb
@@ -3,21 +3,37 @@ require 'spec_helper'
 describe Rack::GitSha do
   include Rack::Test::Methods
 
-  def app
-    Rack::GitSha.new
-  end
-
   describe "GET /" do
-    before do
-      get '/sha'
-    end
+    context "with no path" do
+      def app
+        Rack::GitSha.new
+      end
 
-    it "is successful" do
-      last_response.status.should == 200
-    end
+      before do
+        get '/'
+      end
 
-    it "serves the current git commit sha" do
-      last_response.body.should == `git rev-parse HEAD`
+      it "is successful" do
+        last_response.status.should == 200
+      end
+
+      it "serves the current git commit sha" do
+        last_response.body.should == `git rev-parse HEAD`
+      end
+
+      context "with path" do
+        def app
+          Rack::GitSha.new File.expand_path('../fixtures', __FILE__)
+        end
+
+        before do
+          get '/'
+        end
+
+        it "uses the REVISION file" do
+          last_response.body.should == "git-sha-goes-here\n"
+        end
+      end
     end
   end
 end
